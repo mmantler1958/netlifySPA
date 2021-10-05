a3211470c668ef54179eb7b30ae913946b193123
Martin Mantler <mmantler@gmail.com>
2021-10-05 15:27:10 -0500

    Netlify SPA Example With State

---
+++ index.js
@@ -0,0 +1,32 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo(window.location.origin);
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": (params) => {
+      let page = capitalize(params.page);
+      render(state[page]);
+    },
+  })
+  .resolve();
+
+function render(st) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(st)}
+  ${Nav(state.Links)}
+  ${Main(st)}
+  ${Footer()}
+  `;
+
+  router.updatePageLinks();
+}
+
+// add menu toggle to bars icon in nav bar
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
