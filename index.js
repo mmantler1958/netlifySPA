1dc83cc2b4aa3e02a7ffd6d5a006ccff6c1bddb5
Martin Mantler <mmantler@gmail.com>
2021-10-06 18:42:36 -0500

    Ready for Netlify

--- index.js
+++ index.js
@@ -2,16 +2,50 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = new Navigo(window.location.origin);
+
+router.hooks({
+  before: (done, params) => {
+    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
+    if (page === "Home") {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`)
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    }
+  }
+});
 
 router
   .on({
     "/": () => render(state.Home),
-    ":page": (params) => {
+    ":page": params => {
       let page = capitalize(params.page);
       render(state[page]);
-    },
+    }
   })
   .resolve();
 
@@ -24,9 +58,24 @@ function render(st) {
   `;
 
   router.updatePageLinks();
+
+  addEventListeners(st);
 }
 
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+function addEventListeners(st) {
+  // add event listeners to Nav items for navigation
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
+      event.preventDefault();
+      render(state[event.target.title]);
+    })
+  );
+
+  // add menu toggle to bars icon in nav bar
+  document
+    .querySelector(".fa-bars")
+    .addEventListener("click", () =>
+      document.querySelector("nav > ul").classList.toggle("hidden--mobile")
+    );
+}
